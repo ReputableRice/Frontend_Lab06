e044b8dbadf6a43f26c4439b581f98bfa2597921
Jackie T <jackiehtruong@outlook.com>
2024-10-21 13:16:55 -0700

    checkbox disabled function

--- src/components/Tasks/Task.jsx
+++ src/components/Tasks/Task.jsx
@@ -5,7 +5,10 @@ export default function Task({ task, handleDeletion, index, handleCheck, isCompl
     return (
         <>
             <div className="task">
-                <input type="checkbox" onChange={() => handleCheck(index)} checked={isComplete} />
+                <input type="checkbox"
+                    onChange={() => handleCheck(index)}
+                    checked={isComplete}
+                    disabled={isComplete} />
                 <h3 style={{ textDecoration: isComplete ? "line-through" : "none" }}>{task}</h3>
                 <button onClick={() => handleDeletion(index)}>Remove</button>
             </div>
